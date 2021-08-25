2ca272fc78f9b187cca8515061b45d9467aa08ad
Chandler Liang <chandler.liang123@gmail.com>
2021-08-25 18:23:39 +1000

    created api routes for products

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -4,15 +4,34 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 // The `/api/products` endpoint
 
 // get all products
-router.get('/', (req, res) => {
-  // find all products
-  // be sure to include its associated Category and Tag data
+router.get('/', async (req, res) => {
+  // find all products include its associated Category and Tag data
+  try {
+    const productData = await Product.findAll({
+      include: [{model: Category}, {model: Tag}],
+    });
+  
+    res.status(200).json(productData);
+
+  } catch {
+    res.status(500).json(err);
+  }
+  
 });
 
 // get one product
-router.get('/:id', (req, res) => {
-  // find a single product by its `id`
-  // be sure to include its associated Category and Tag data
+router.get('/:id', async (req, res) => {
+  // find a single product by its `id` include its associated Category and Tag data
+  try {
+    const product = await Product.findByPk(req.params.id, {
+      include: [{model: Category}, {model: Tag}],
+    });
+  
+    res.status(200).json(product);
+  } catch {
+    res.status(500).json(err);
+  }
+  
 });
 
 // create new product
@@ -89,8 +108,16 @@ router.put('/:id', (req, res) => {
     });
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete one product by its `id` value
+  const product = await Product.findByPk(req.params.id);
+
+  if (!product) {
+    res.json({message: "This product does not exist"});
+    return
+  }
+
+  product.destroy().then(res.status(200).json(product));
 });
 
 module.exports = router;
